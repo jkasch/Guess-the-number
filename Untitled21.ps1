1526641016c7d3e636b53dee1b6cbb53717fb1f1
Jennifer Kasch <116533132+jkasch@users.noreply.github.com>
2023-10-19 21:28:44 -0500

    Add files via upload

---
+++ Untitled21.ps1
@@ -0,0 +1,22 @@
+﻿'Please guess the number between 1 and 5'
+
+$NumberGuess = Read-Host
+
+$RandomNumber = Get-Random -Minimum 1 -Maximum 6
+
+while($NumberGuess -ne $RandomNumber)
+{
+
+    if($NumberGuess -lt $RandomNumber)
+    {
+        'Guess again, your number ' + $NumberGuess + ' is too low'
+    } 
+    else
+    {
+        'Guess again, your number ' + $NumberGuess + ' is too high'
+    }
+    $NumberGuess = Read-Host
+    
+}
+
+'YESSSSS, you guessed it, the number is ' + $RandomNumber
